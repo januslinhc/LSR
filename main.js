e446a87f2853ab4a6a096c83d223b5e6ef634ac8
h1994st <h1994st@gmail.com>
2015-11-22 18:04:36 +0800

    Update
    1. Closure
    2. Update UI
    3. TODO: Add Network Visulization

--- main.js
+++ main.js
@@ -5,99 +5,149 @@
  * @version 1.2
  */
 
-var graph = {};
+$(document).ready(function() {
+    var graph = {};
+    var source;
 
-function display(text) {
-    document.getElementById('feedback').innerText = text;
-}
+    $('#input').change(function (event) {
+        if (this.files.length > 0) {
+            readFile(this, function (text) {
+                document.getElementById('file-content').innerText = text; // 显示文件
+                graph = parseGraph(text);
+                console.log(graph);
+            });
 
-function compute() {
-    var sourceInput = document.getElementById("source");
-    var fileInput = document.getElementById('input');
-
-    if (fileInput.files.length > 0 && sourceInput.value !== "") {
-        readFile(fileInput, function(text) {
-            document.getElementById('file-content').innerText = text;
-            graph = parseGraph(text);
-            var source = sourceInput.value;
-            console.log(source, graph);
-            render(graph, source);
-        });
-    } else {
-        display("Input is empty");
-    }
-}
-
-function getResultStr(result) {
-    var retulrStr = '';
-
-    for (var key in result) {
-        var node = key;
-        var distance = result[key].distance;
-        var path = result[key].path;
-
-        if (distance === Number.MAX_VALUE || distance === 1.7976931348623157e+308) continue; // 不输出无法到达的点
-
-        var pathStr = '';
-        for (var i = 0; i < path.length; i++) {
-        pathStr += (path[i] + ' > ');
+            $('#source').attr('disabled', false); // 允许输入source
+        } else {
+            display("Input is empty");
         }
-        pathStr += node;
-        retulrStr += (node + ': ' + pathStr + ', ' + distance + '\n');
+    });
+
+    // Compute
+    $('#compute-all-btn').click(function (event) {
+        event.preventDefault();
+
+        compute();
+    });
+
+    // Single Step
+    $('#single-step-btn').click(function (event) {
+        event.preventDefault();
+
+    });
+
+    // Add / Update Edge
+    $('#update-edge-btn').click(function (event) {
+        event.preventDefault();
+
+        updateEdge();
+    });
+
+    // Add New Node
+    $('#add-new-node-btn').click(function (event) {
+        event.preventDefault();
+
+        addNewNode();
+    });
+
+    // Delete Old Node
+    $('#delete-node-btn').click(function (event) {
+        event.preventDefault();
+
+        deleteNode();
+    });
+
+    function compute() {
+        var sourceInput = document.getElementById("source");
+        var fileInput = document.getElementById('input');
+
+        if (fileInput.files.length > 0 && sourceInput.value !== "") {
+            readFile(fileInput, function(text) {
+                document.getElementById('file-content').innerText = text;
+                graph = parseGraph(text);
+                var source = sourceInput.value;
+                console.log(source, graph);
+                render(graph, source);
+            });
+        } else {
+            display("Input is empty");
+        }
     }
 
-    return retulrStr;
-}
-
-function render(graph, source) {
-    var problem = new Dijkstra(graph, source);
-    var result = problem.solve();
-
-    var resultStr = getResultStr(result);
-    display(resultStr);
-}
-
-// Example of using update.js
-// load update.js before this
-
-var update = updateExport(display);
-
-// Re-export
-function updateEdge() {
-    var src = document.getElementById('update-edge-src').value;
-    var dest = document.getElementById('update-edge-dest').value;
-    var weight = document.getElementById('update-edge-weight').value;
-    var source = document.getElementById("source").value;
-
-    if(src.length && dest.length && weight.length && source.length) {
-        update.updateEdge(graph, src, dest, parseInt(weight));
-        render(graph, source);
-    } else {
-        display("error: you don't enter everything!");
+    function display(text) {
+        document.getElementById('feedback').innerText = text;
     }
-}
 
-function addNewNode() {
-    var nodeId = document.getElementById('add-new-node-id').value;
-    var source = document.getElementById("source").value;
+    function getResultStr(result) {
+        var retulrStr = '';
 
-    if(nodeId.length & source.length) {
-        update.addNode(graph, nodeId, {});
-        render(graph, source);
-    } else {
-        display("error: you don't enter everything!");
+        for (var key in result) {
+            var node = key;
+            var distance = result[key].distance;
+            var path = result[key].path;
+
+            if (distance === Number.MAX_VALUE || distance === 1.7976931348623157e+308) continue; // 不输出无法到达的点
+
+            var pathStr = '';
+            for (var i = 0; i < path.length; i++) {
+            pathStr += (path[i] + ' > ');
+            }
+            pathStr += node;
+            retulrStr += (node + ': ' + pathStr + ', ' + distance + '\n');
+        }
+
+        return retulrStr;
     }
-}
 
-function deleteNode() {
-    var nodeId = document.getElementById('delete-node-id').value;
-    var source = document.getElementById("source").value;
+    function render(graph, source) {
+        var problem = new Dijkstra(graph, source);
+        var result = problem.solve();
 
-    if(nodeId.length & source.length) {
-        update.deleteNode(graph, nodeId);
-        render(graph, source);
-    } else {
-        display("error: you don't enter everything!");
+        var resultStr = getResultStr(result);
+        display(resultStr);
     }
-}
 
+    // Example of using update.js
+    // load update.js before this
+
+    var update = updateExport(display);
+
+    // Re-export
+    function updateEdge() {
+        var src = document.getElementById('update-edge-src').value;
+        var dest = document.getElementById('update-edge-dest').value;
+        var weight = document.getElementById('update-edge-weight').value;
+        var source = document.getElementById("source").value;
+
+        if(src.length && dest.length && weight.length && source.length) {
+            update.updateEdge(graph, src, dest, parseInt(weight));
+            render(graph, source);
+        } else {
+            display("error: you don't enter everything!");
+        }
+    }
+
+    function addNewNode() {
+        var nodeId = document.getElementById('add-new-node-id').value;
+        var source = document.getElementById("source").value;
+
+        if(nodeId.length & source.length) {
+            update.addNode(graph, nodeId, {});
+            render(graph, source);
+        } else {
+            display("error: you don't enter everything!");
+        }
+    }
+
+    function deleteNode() {
+        var nodeId = document.getElementById('delete-node-id').value;
+        var source = document.getElementById("source").value;
+
+        if(nodeId.length & source.length) {
+            update.deleteNode(graph, nodeId);
+            render(graph, source);
+        } else {
+            display("error: you don't enter everything!");
+        }
+    }
+});
--- src/Dijkstra.js
+++ src/Dijkstra.js
@@ -7,165 +7,173 @@
 
 'use strict';
 
-// Logger
-function logger(type, text) {
-  if (type === 'error') console.error('[error] ' + text);
-  else if (type === 'info') console.log('[info] ' + text);
-}
-
-// Dijkstra
-function Dijkstra(graph, source) {
-  var _graph = graph;
-  var _source = source;
-  var _solver;
-
-  // 有可能的话，初始化solver
-  if (graph && source) {
-    logger('info', 'Initializing solver.');
-    initSolver();
+(function (_window) {
+  // Logger
+  function logger(type, text) {
+    if (type === 'error') console.error('[error] ' + text);
+    else if (type === 'info') console.log('[info] ' + text);
   }
 
-  // Setter and Getter
-  this.setGraph = function (graph) {
-    _graph = graph;
-    _source = ''; // 清空source
-  };
-  this.getGraph = function () {
-    return _graph;
-  };
-  this.setSource = function (source) {
-    if (!_graph) { // 图为空
-      logger('error', 'Empty graph. Please set graph first.');
-      return false;
-    }
-    if (!(source in _graph)) { // source不在图中
-      logger('error', 'Unknown source.');
-      return false;
-    }
-    logger('info', 'source: ' + source);
-    _source = source; // 设置source
+  // Dijkstra
+  _window.Dijkstra = function (graph, source) {
+    var _graph = graph;
+    var _source = source;
+    var _solver;
 
-    logger('info', 'Initializing solver.');
-    initSolver(); // 初始化solver
-    return true;
+    // 有可能的话，初始化solver
+    if (graph && source) {
+      logger('info', 'Initializing solver.');
+      initSolver();
+    }
+
+    // Setter and Getter
+    this.setGraph = function (graph) {
+      _graph = graph;
+      _source = ''; // 清空source
+      _solver = undefined; // 清除老的solver
+      return true;
+    };
+    this.getGraph = function () {
+      return _graph;
+    };
+    this.setSource = function (source) {
+      if (!_graph) { // 图为空
+        logger('error', 'Empty graph. Please set graph first.');
+        return false;
+      }
+      if (!(source in _graph)) { // source不在图中
+        logger('error', 'Unknown source.');
+        return false;
+      }
+      logger('info', 'source: ' + source);
+      _source = source; // 设置source
+
+      logger('info', 'Initializing solver.');
+      initSolver(); // 初始化solver
+      return true;
+    };
+    this.getSource = function () {
+      return _source;
+    };
+    this.getResult = function () {
+      if (!_solver) { // solver未初始化
+        logger('error', 'Uninitialized solver.');
+        return;
+      }
+      return _solver.getResult();
+    };
+
+    function initSolver() {
+      if (!_graph) { // graph没有设置
+        logger('error', 'Cannot init solver. Empty graph.');
+        return false;
+      }
+      if (!_source) { // source没有设置
+        logger('error', 'Cannot init solver. Empty source.');
+        return false;
+      }
+      if (!(_source in _graph)) { // source不在图中
+        logger('error', 'Unknown source. Please set a valid source.');
+        return false;
+      }
+
+      _solver = new DijkstraSolver(_graph, _source); // 创建solver
+      logger('info', 'Solver initialized.');
+
+      return true;
+    }
+
+    this.singleStep = function () {
+      if (!_solver) { // solver未初始化
+        logger('error', 'Uninitialized solver.');
+        return false;
+      }
+
+      return _solver.next();
+    };
+
+    this.solve = function () {
+      if (!_solver) { // solver未初始化
+        logger('error', 'Uninitialized solver.');
+        return false;
+      }
+
+      return _solver.solve();
+    };
   };
-  this.getSource = function () {
-    return _source;
-  };
-  this.getResult = function () {
-    if (!_solver) { // solver未初始化
-      logger('error', 'Uninitialized solver.');
+
+  // Dijkstra Solver
+  function DijkstraSolver(graph, source) {
+    if (!graph || !source) {
+      logger('error', 'Empty graph or soruce.');
       return;
     }
-    return _solver.getResult();
-  };
 
-  function initSolver() {
-    if (!_graph) { // graph没有设置
-      logger('error', 'Cannot init solver. Empty graph.');
-      return false;
-    }
-    if (!_source) { // source没有设置
-      logger('error', 'Cannot init solver. Empty source.');
-      return false;
-    }
-    if (!(_source in _graph)) { // source不在图中
-      logger('error', 'Unknown source. Please set a valid source.');
-      return false;
-    }
+    // Default Values
+    var _done = false;
+    var _queue = [];
+    var _queueIndex = 0;
+    var _result = {};
 
-    _solver = new Solver(_graph, _source); // 创建solver
-    logger('info', 'Solver initialized.');
-
-    return true;
-  }
-
-  this.singleStepSolve = function () {
-    if (!_solver) { // solver未初始化
-      logger('error', 'Uninitialized solver.');
-      return false;
-    }
-
-    return _solver.next();
-  };
-
-  this.solve = function () {
-    if (!_solver) { // solver未初始化
-      logger('error', 'Uninitialized solver.');
-      return false;
-    }
-
-    return _solver.solve();
-  };
-}
-
-// Dijkstra Solver
-function Solver(graph, source) {
-  if (!graph || !source) {
-    logger('error', 'Empty graph or soruce.');
-    return;
-  }
-
-  var __done = false;
-  var __queue = [];
-  var __queueIndex = 0;
-  var __source = source;
-  var __graph = graph;
-  var __result = {};
-  __queue.push(__source);
-
-  init();
-
-  // 初始化result标记
-  function init() {
-    for (var key in __graph) {
-      __result[key] = {
-        distance: Number.MAX_VALUE ? Number.MAX_VALUE : 1.7976931348623157e+308,
-        path: []
-      };
-    }
-
-    // source的距离
-    __result[__source].distance = 0;
-  }
-
-  // 重置
-  this.reset = function () {
-    __done = false;
-    __queue = [];
-    __queueIndex = 0;
-    __result = {};
-    __queue.push(__source);
+    // Constructor
+    var _source = source;
+    var _graph = graph;
+    _queue.push(_source);
 
     init();
-  };
 
-  this.next = function () {
-    if (__done) return true; // 已完成
-
-    var node = __queue[__queueIndex++];
-    for (var target in __graph[node]) {
-      if (__queue.indexOf(target) === -1) __queue.push(target); // 未遍历，且还不在queue中
-      else if (__queue.indexOf(target) < __queueIndex) continue; // 遍历过了
-
-      if (__graph[node][target] + __result[node].distance < __result[target].distance) {
-        __result[target].distance = __graph[node][target] + __result[node].distance;
-        __result[target].path = __result[node].path.concat(node);
+    // 初始化result标记
+    function init() {
+      for (var key in _graph) {
+        _result[key] = {
+          distance: Number.MAX_VALUE ? Number.MAX_VALUE : 1.7976931348623157e+308,
+          path: []
+        };
       }
+
+      // source的距离
+      _result[_source].distance = 0;
     }
 
-    if (__queue.length === __queueIndex) __done = true; // 刚完成
+    // 重置
+    this.reset = function () {
+      _done = false;
+      _queue = [];
+      _queueIndex = 0;
+      _result = {};
+      _queue.push(_source);
 
-    return __done;
-  };
+      init();
+    };
 
-  this.getResult = function () {
-    return __result;
-  };
+    // 返回值表示是否可以继续下一步
+    this.next = function () {
+      if (_done) return false; // 已完成
 
-  this.solve = function () {
-    if (__done) return __result; // 已完成
-    while (!this.next()) ; // 单步到底
-    return __result;
-  };
-}
+      var node = _queue[_queueIndex++];
+      for (var target in _graph[node]) {
+        if (_queue.indexOf(target) === -1) _queue.push(target); // 未遍历，且还不在queue中
+        else if (_queue.indexOf(target) < _queueIndex) continue; // 遍历过了
+
+        if (_graph[node][target] + _result[node].distance < _result[target].distance) {
+          _result[target].distance = _graph[node][target] + _result[node].distance;
+          _result[target].path = _result[node].path.concat(node);
+        }
+      }
+
+      if (_queue.length === _queueIndex) _done = true; // 刚完成
+
+      return !_done;
+    };
+
+    this.getResult = function () {
+      return _result;
+    };
+
+    this.solve = function () {
+      if (_done) return _result; // 已完成
+      while (this.next()) ; // 单步到底
+      return _result;
+    };
+  }
+})(window);
